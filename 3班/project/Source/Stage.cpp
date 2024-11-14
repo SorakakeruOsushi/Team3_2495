b6e4c8bd133900db6c83b8dad0d1bea3093da114
SueKazuma <sdt2495gp0009@edu.sca.ac.jp>
2024-11-14 12:29:22 +0900

    マップサイズも30x30に対応

--- 3班/project/Source/Stage.cpp
+++ 3班/project/Source/Stage.cpp
@@ -4,7 +4,7 @@
 
 const int WIDTH = 24;		//ステージ 幅
 const int HEIGHT = 24;		//ステージ 高
-const int CHIP_SIZE = 20;   //チップサイズ
+const int CHIP_SIZE = 30;   //チップサイズ
 int map[HEIGHT][WIDTH];
 
 Stage::Stage()
@@ -26,10 +26,10 @@ void Stage::Draw()
 	//マップタイル表示
 	for (int j = 0; j < HEIGHT; j++)// 縦「j」
 	{
-		int y = j * CHIP_SIZE + (CHIP_SIZE*5);
+		int y = j * CHIP_SIZE + (CHIP_SIZE*0);
 		for (int i = 0; i < WIDTH; i++)// 幅「i」
 		{
-			int x = i * CHIP_SIZE + (CHIP_SIZE*5);
+			int x = i * CHIP_SIZE + (CHIP_SIZE*0);
 
 			// 空マス「Empty.png」を描画
 			DrawGraph(x, y, emptyImage, TRUE);
