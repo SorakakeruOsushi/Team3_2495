9e95b7433d8ab984deb1ede71a667cc4265d702e
末一馬 <sdt2495gp0009@edu.sca.ac.jp>
2024-11-28 10:34:08 +0900

    とりあえずタイトル画面表示だけ

--- 3班/project/Source/TitleScene.cpp
+++ 3班/project/Source/TitleScene.cpp
@@ -1,12 +1,16 @@
 #include "TitleScene.h"
 #include <DxLib.h>
+#include <cassert>
 
 TitleScene::TitleScene()
 {
+	titleImage = LoadGraph("data/image/Title.JPG");
+		assert(titleImage > 0);
 }
 
 TitleScene::~TitleScene()
 {
+	DeleteGraph(titleImage);
 }
 
 void TitleScene::Update()
@@ -27,4 +31,5 @@ void TitleScene::Draw()
 {
 	DrawString(0, 0, "TITLE SCENE", GetColor(255,255,255));
 	DrawString(100, 400, "Push [SPACE]Key To Play", GetColor(255, 255, 255));
+	DrawGraph(0, 0, titleImage, TRUE);
 }
