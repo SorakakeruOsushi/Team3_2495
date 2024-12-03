8f2d19f9ccaa50c9d39b2930ab561be0982a1370
hata <sdt22csc0012@edu.sca.ac.jp>
2024-12-03 10:35:20 +0900

    ブロックの左右移動、Sキーでの加速、ミノの形のランダム生成を仮で追加

--- 3班/project/Source/Block.cpp
+++ 3班/project/Source/Block.cpp
@@ -3,6 +3,7 @@
 #include "player.h"
 #include <cassert>
 
+
 //やる事
 //横移動（押したときだけ）→回転→当たり判定→ランダム生成（NEXT込み）
 
@@ -135,9 +136,9 @@ SHAPE shapes[SHAPE_MAX] = {
 
 Block::Block()
 {
-	hImage = LoadGraph("data/image/Tmino_30.png");
-	assert(hImage > 0);
-
+	isMovedLeft = false;
+	isMovedRight = false;
+	
 	position.x = 0;
 	position.y = 0;
 
@@ -145,8 +146,31 @@ Block::Block()
 
 	time = 1.3f;
 	counter = 0.0f;
+	quickCount = 17.0f;
 
-	rand() % 4; // ランダムなミノが選ばれる
+	for (int i = 0; i < 10; i++) {
+		rand();//乱数表の初期値10個を捨てる(よりランダムにするため)
+	}
+
+	// ランダムなミノが選ばれる
+	switch (rand() % 4) {
+	case 0:
+		hImage = LoadGraph("data/image/Tmino_30.png");
+		assert(hImage > 0);
+		break;
+	case 1:
+		hImage = LoadGraph("data/image/Jmino_30.png");
+		assert(hImage > 0);
+		break;
+	case 2:
+		hImage = LoadGraph("data/image/Lmino_30.png");
+		assert(hImage > 0);
+		break;
+	case 3:
+		hImage = LoadGraph("data/image/Omino_30.png");
+		assert(hImage > 0);
+		break;
+	} 
 }
 
 Block::~Block()
@@ -159,10 +183,36 @@ Block::~Block()
 void Block::Update()
 {
 	//ブロックを落とす
-	counter += Time::DeltaTime();
+	if (CheckHitKey(KEY_INPUT_S)) {
+		 counter += Time::DeltaTime()*quickCount;
+		//Sが押されていたらquickCount倍早くcountが進む＝早く落ちる
+	}
+	else {
+		counter += Time::DeltaTime();//押されていなければそのまま
+	}
 	if (counter >= time) {
 		position.y += blockSize;
 		counter = 0.0f;
+	}
+	//左に移動
+	if (CheckHitKey(KEY_INPUT_A)) {
+		if (isMovedLeft == false) {
+			position.x -= blockSize;
+			isMovedLeft = true;
+		}
+	}
+	else {
+		isMovedLeft = false;
+	}
+	//右に移動
+	if (CheckHitKey(KEY_INPUT_D)) {
+		if (isMovedRight == false) {
+			position.x += blockSize;
+			isMovedRight = true;
+		}
+	}
+	else {
+		isMovedRight = false;
 	}
 
 }
--- 3班/project/Source/Block.h
+++ 3班/project/Source/Block.h
@@ -2,6 +2,7 @@
 #include "../Library/GameObject.h"
 #include "Vector2.h"
 
+
 class Block :
     public GameObject
 {
@@ -22,5 +23,9 @@ public:
 
 	float time;
 	float counter;
+	float quickCount;//S‚ğ‰Ÿ‚µ‚Ä‚¢‚é‚Écounter‚ğ‘‚â‚·”{—¦
+
+	bool isMovedLeft;//‰E‚ÉˆÚ“®‚µ‚½‚©
+	bool isMovedRight;//¶‚ÉˆÚ“®‚µ‚½‚©
 };
 
